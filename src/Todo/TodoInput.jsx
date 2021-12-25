e65a99dce9a08e2da5035688f9a5b0cbcdc4e042
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-25 13:08:35 +0530

    todo-list-using-material-ui

--- src/Todo/TodoInput.jsx
+++ src/Todo/TodoInput.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
-
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 function TodoInput({ onAdd }) {
   const [state, setState] = useState("");
 
   return (
     <div>
-      <input
+      {/* <input
+       
+      /> */}
+      <TextField
+        label="Add Events Here"
         value={state}
         onChange={(e) => setState(e.target.value)}
         placeholder="Add something"
+        color="secondary"
+        focused
       />
-      <button
+      <Button
+        variant="contained"
+        style={{ marginTop: "10px" }}
         onClick={() => {
           onAdd(state);
           setState("");
         }}
       >
         ADD
-      </button>
+      </Button>
     </div>
   );
 }
